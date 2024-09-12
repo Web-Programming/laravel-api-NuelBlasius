7f1933a6b5d295ce922034e5c20565a8ee41d613
NuelBlasius <161033492+NuelBlasius@users.noreply.github.com>
2024-09-12 19:51:49 +0700

    Update DatabaseSeeder.php

--- laravel/app-kitobiso/database/seeders/DatabaseSeeder.php
+++ laravel/app-kitobiso/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Donation;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,21 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory(10)->create([
+            'title' => 'Bantuan Kemanusiaan',
+            'des' => 'Bantuan untuk korban bencana alam',
+            'image' => 'bantuan-kemanusian.jpg',
+            'progress' => '0',
+            'duration' => '1 bulan',
+            'collected' => 0,
+            'target' => 1000000,
+            'user_id' => 1,
+        ]);
+
+        Donation::create(attributes:[
+            'amount' => 100000,
+            'funding_id' => 1,
+            'user_id' => 2,
         ]);
     }
 }
